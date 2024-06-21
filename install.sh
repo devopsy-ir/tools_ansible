f396a2680313ca2dc451e20cd87274d187682abb
محسن عباسی <developer961@gmail.com>
2024-06-21 22:02:11 +0330

    install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ domain=$2
 
 mkdir -p ${tools_dir}/nexus
 
+sed -i -E "s/domain:.*/domain: ${domain}/g" ./hosts.yml
+sed -i -E "s/tools_dir:.*/tools_dir: ${tools_dir}/g" ./hosts.yml
+
 # Nginx - Nexus
 echo "Up Nginx ..."
-sed -i -E "s/domain:.*/domain: ${domain}/g" ./nginx/ansible/hosts.yml
-sed -i -E "s/tools_dir:.*/tools_dir: ${tools_dir}/g" ./nginx/ansible/hosts.yml
-cd ./nginx/ansible/
 ansible-playbook -i hosts.yml nginx_nexus.yml
 
 # Nexus
@@ -15,10 +15,11 @@ echo "Copy Nexus docker images ..."
 rsync  -qazhPW --inplace  --progress ./nexus/volumes ${tools_dir}/nexus/
 
 echo "Up Nexus ..."
-sed -i -E "s/tools_dir:.*/tools_dir: ${tools_dir}/g" ./nexus/ansible/hosts.yml
-cd ./nexus/ansible/
 ansible-playbook -i hosts.yml nexus.yml
 
 # Nginx - Matrix
 sleep 120
 ansible-playbook -i hosts.yml nginx_matrix.yml
+
+# Matrix
+ansible-playbook -i hosts.yml matrix.yml
